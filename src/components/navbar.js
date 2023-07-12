a40dc9699304e737f3c2b0cb9592be168597a553
Hannah Santoyo <hannahrichardxo@gmail.com>
2023-07-11 20:29:25 -0700

    created navbar and some file structure stuff

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+const TopNav = () => {
+  return (
+    <>
+      <img alt="cows" src="https://www.alltech-e-co2.com/wp-content/uploads/2016/10/cow-signals-header.jpg"></img>
+      <nav className='navbar' >
+        <a href='/'>Home</a>
+        <a href='/about'>About Me</a>
+        <a href='/projects'>Projects</a>
+      </nav>
+    </>
+  );
+}
+
+export default TopNav;
